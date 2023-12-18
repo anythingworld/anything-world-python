1188fc5bb923751cd4c90dfce1db6aa1cd24623f
Vilson Vieira <vilson@void.cc>
2023-12-18 17:52:33 -0300

    Finish working version

---
+++ anything_world/api_client.py
@@ -0,0 +1,179 @@
+import asyncio
+from typing import Optional
+from dotenv import load_dotenv
+
+from .utils import get_env, read_files, create_form_data, send_request
+
+
+class AWClient:
+    """
+    Anything World API Client
+
+                        .-. .-.    _                                   .-.     .-.
+                       .' `.: :   :_;                                  : :     : :
+     .--.  ,-.,-..-..-.`. .': `-. .-.,-.,-. .--.   .-..-..-. .--. .--. : :   .-' :
+    ' .; ; : ,. :: :; : : : : .. :: :: ,. :' .; :  : `; `; :' .; :: ..': :_ ' .; :
+    `.__,_;:_;:_;`._. ; :_; :_;:_;:_;:_;:_;`._. ;  `.__.__.'`.__.':_;  `.__;`.__.'
+                  .-. :                     .-. :
+                  `._.'                     `._.'
+
+   Provides an interface to the Anything World API. It allows to send requests to the
+   API to animate 3D models and retrieve them once they are done.
+   """
+
+    # Stage names that identify the end of a process for a given endpoint name
+    _finished_stages = {
+        "animate": "thumbnails_generation_finished"
+    }
+
+    def __init__(self, api_key: Optional[str] = None):
+        """
+        Initialize an instance of AWClient.
+
+        :param api_key: str, API key to use for requests. If not provided, it will
+            be read from the environment variable AW_API_KEY.
+        """
+        load_dotenv()
+        self.api_key = api_key if api_key else get_env('AW_API_KEY')
+        self.api_url = get_env('AW_API_URL')
+        self.api_key = get_env('AW_API_KEY')
+        self.polling_url = get_env('AW_POLLING_URL')
+
+
+    async def animate(
+            self,
+            files_dir: str,
+            model_name: str,
+            model_type: str,
+            is_symmetric: bool = True) -> dict:
+        """
+        Asynchronously sends a request to animate a model.
+
+        This function reads files from the specified directory, creates form data from the files and additional data,
+        and sends a POST request to the API to animate the model.
+
+        :param files_dir: str, the directory where the files to be animated are located.
+        :param model_name: str, the name of the model to be animated.
+        :param model_type: str, the type of the model to be animated.
+        :param is_symmetric: bool, optional, a flag indicating whether the model is symmetric. Defaults to True.
+
+        :return: dict, the JSON response from the API decoded as a dict.
+        """
+        data = {
+            "api_key": self.api_key,
+            "model_name": model_name,
+            "model_type": model_type,
+            "symmetry": "true" if is_symmetric else "false"
+        }
+        form_data = create_form_data(read_files(files_dir), data)
+        return await send_request(
+            url=f"{self.api_url}/run_all",
+            method="POST",
+            data=form_data
+        )
+
+
+    async def get_animated_model(
+            self,
+            model_id: str,
+            waiting_time: Optional[int] = 5,
+            verbose: Optional[bool] = False) -> dict:
+        """
+        Asynchronously retrieves an animated model by polling.
+
+        This function sends a GET request to the API to retrieve the animated model. It polls the API until the model
+        reaches the finished stage expected for the /animate endpoint, waiting a specified amount of time
+        between each request.
+
+        :param model_id: str, the ID of the model to retrieve.
+        :param waiting_time: int, optional, the amount of time to wait between each request in seconds. Defaults to 5.
+        :param verbose: bool, optional, a flag indicating whether to print detailed information about each request.
+            Defaults to False.
+        :return: dict, the JSON response from the API decoded as a dict.
+        """    
+        return await self.get_model_by_polling(
+            model_id,
+            expected_stage=self._finished_stages["animate"],
+            waiting_time=waiting_time,
+            verbose=verbose)
+
+
+    async def is_animation_done(self, model_id: str) -> bool:
+        """
+        Checks if the animation of a model is done.
+
+        This function sends a request to the API to check if the animation of the specified model is done.
+
+        :param model_id: str, the ID of the model to check.
+        :return: bool, True if the animation is done, False otherwise.
+        """
+        return await self._is_model_done(model_id, "animate")
+
+
+    async def get_model(self, model_id: str) -> dict:
+        """
+        Retrieves a model.
+
+        This function sends a GET request to the API to retrieve the specified model. If the response is a list with
+        one item, it returns the item. Otherwise, it returns the whole response.
+
+        :param model_id: str, the ID of the model to retrieve.
+        :return: dict, the JSON response from the API decoded as a dict.
+        """
+        params = {
+            'key': self.api_key,
+            'id': model_id,
+            'stage': 'done',
+            'staging': 'true'
+        }
+        res = await send_request(
+            url=self.polling_url,
+            method="GET",
+            params=params)
+        if isinstance(res, list) and len(res) == 1:
+            return res[0]
+        return res
+
+
+    async def get_model_by_polling(
+            self,
+            model_id: str,
+            expected_stage: str,
+            waiting_time: Optional[int] = 5,
+            verbose: Optional[bool] = False) -> dict:
+        """
+        Retrieves a model by polling until it reaches the expected stage.
+
+        This function sends a GET request to the API to retrieve the specified model. It polls the API until the model
+        reaches the expected stage, waiting a specified amount of time between each request.
+
+        :param model_id: str, the ID of the model to retrieve.
+        :param expected_stage: str, the stage that the model is expected to reach.
+        :param waiting_time: int, optional, the amount of time to wait between each request in seconds. Defaults to 5.
+        :param verbose: bool, optional, a flag indicating whether to print detailed information about each request.
+            Defaults to False.
+        :return: dict, the JSON response from the API decoded as a dict.
+        """
+        attempt_count = 0
+
+        while True:
+            attempt_count += 1
+            status_prefix = f"Polling attempt #{attempt_count}."
+            model_json = await self.get_model(model_id)
+            if "stage" in model_json:
+                model_stage = model_json["stage"]
+                if model_stage == expected_stage:
+                    if verbose:
+                        print(f"{status_prefix} Done.")
+                    return model_json
+            else:
+                if verbose:
+                    print(f"{status_prefix} Model is not ready yet...")
+            await asyncio.sleep(waiting_time)
+
+    async def _is_model_done(self, model_id: str, endpoint: str) -> bool:
+        res = await self.get_model(model_id)
+        if "stage" in res:
+            return res["stage"] == self._finished_stages[endpoint]
+        return False
+
---
+++ anything_world/utils.py
@@ -0,0 +1,117 @@
+import os
+import aiohttp
+import aiofiles
+import magic
+from typing import Optional
+
+
+def get_env(key: str) -> Optional[str]:
+    """
+    Retrieves the value of an environment variable.
+
+    This function retrieves the value of the specified environment variable. If the environment variable is not set,
+    it raises an exception.
+
+    :param key: str, the name of the environment variable.
+    :return: str, optional, the value of the environment variable, or None if the environment variable is not set.
+    :raises Exception: If the environment variable is not set.
+    """
+    value = os.getenv(key)
+    if not value:
+        raise Exception(f"Environment variable {key} is not set")
+    return value
+
+
+def read_files(files_dir: str) -> list:
+    """
+    Read files from a directory (or single file), returning a tuple with
+    filename, full path to file and its mimetype (inferred by its
+    extension-only for now).
+
+    :param files_dir: str, path to asset directory (or single asset file)
+    :return: list, files as tuples (file name, local path, mime type)
+    """
+    files_to_get = []
+    if os.path.isdir(files_dir):
+        files_to_get = os.listdir(files_dir)
+    else:
+        files_to_get = [files_dir]
+    files = []
+    for filename in files_to_get:
+        if filename == files_dir:
+            file_path = filename
+        else:
+            file_path = os.path.join(files_dir, filename)
+        mime = magic.Magic(mime=True)
+        mimetype = mime.from_buffer(open(file_path, "rb").read(4096))
+        files.append((os.path.basename(file_path), file_path, mimetype))
+    return files
+
+
+def create_form_data(files: list, key_value_data: dict) -> aiohttp.FormData:
+    """
+    Create an instance of `aiohttp.FormData` encoding files (with the
+    right filename and content type) and key-value data, provided as a dict.
+
+    :param files: list, asset files represented as tuple like
+        (filename, filepath, mimetype)
+    :param key_value_data: dict, representing key-value pairs to encode as
+        form-data fields
+    :return: FormData, data to be sent as form-data
+    """
+    data = aiohttp.FormData()
+    for filename, filepath, mimetype in files:
+        data.add_field(
+            filename,
+            open(filepath, 'rb'),
+            filename=filename,
+            content_type=mimetype)
+    for key, value in key_value_data.items():
+        data.add_field(key, value)
+    return data
+
+
+async def download_file(url: str, file_path: str):
+    """
+    Asynchronously downloads a file from a URL.
+
+    This function sends a GET request to the specified URL and writes the response to a file at the specified path.
+
+    :param url: str, the URL of the file to download.
+    :param file_path: str, the path where the downloaded file should be saved.
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url) as resp:
+            if resp.status == 200:
+                f = await aiofiles.open(file_path, mode='wb')
+                await f.write(await resp.read())
+                await f.close()
+
+
+async def send_request(url: str,
+                       method: Optional[str] = "POST",
+                       request_timeout: Optional[int] = 500,
+                       **kwargs) -> dict:
+    """
+    Asynchronously sends a request to a URL.
+
+    This function sends a GET or POST request to the specified URL and returns the response. If the response content
+    type is not "application/json", it raises an exception.
+
+    :param url: str, the URL to send the request to.
+    :param method: str, optional, the HTTP method to use. Defaults to "POST".
+    :param request_timeout: int, optional, the total timeout for the request in seconds. Defaults to 500.
+    :param kwargs: additional keyword arguments to pass to the request.
+    :return: dict, the JSON response from the server encoded as a dict.
+    :raises Exception: If the response content type is not "application/json".
+    """
+    timeout = aiohttp.ClientTimeout(total=request_timeout)
+    async with aiohttp.ClientSession(timeout=timeout) as session:
+        if method == "GET":
+            res = await session.get(url, **kwargs)
+        else:
+            res = await session.post(url, **kwargs)
+        if res.content_type == "application/json":
+            return await res.json()
+        else:
+            raise Exception(f"Unexpected response content type: {res.content_type}")
