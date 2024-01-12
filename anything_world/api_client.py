252593ebc9b159ba990bed64e168bb417c785a4c
Vilson Vieira <vilson@void.cc>
2024-01-12 11:56:45 -0300

    Add support for /anything endpoint

--- anything_world/api_client.py
+++ anything_world/api_client.py
@@ -36,8 +36,115 @@ class AWClient:
         load_dotenv()
         self.api_key = api_key if api_key else get_env('AW_API_KEY')
         self.api_url = get_env('AW_API_URL')
-        self.api_key = get_env('AW_API_KEY')
         self.polling_url = get_env('AW_POLLING_URL')
+
+
+    async def anything(
+            self,
+            model_name: Optional[str] = None,
+            search_query: Optional[str] = None,
+            guid: Optional[int] = None,
+            fuzzy: Optional[bool] = None,
+            include_poly: Optional[bool] = None,
+            include_tags: Optional[bool] = None,
+            include_taxonomy: Optional[bool] = None,
+            include_categories: Optional[bool] = None,
+            vehicle_only: Optional[bool] = None,
+            segmented_only: Optional[bool] = None,
+            rigged_only: Optional[bool] = None,
+            animated_only: Optional[bool] = None,
+            sort: Optional[str] = None,
+            descending: Optional[bool] = None,
+            obj_wrap: Optional[bool] = None,
+            increase_popularity: Optional[bool] = None
+            ) -> dict:
+        """
+        Asynchronously queries the Anything World API for a given model name or search query.
+
+        :param model_name: str, optional, the name of the model to query.
+        :param search_query: str, optional, the search query to query.
+        :param guid: four digit int, optional, GUID of the model if the name/search
+            parameter uses the base name without hashtag and GUID.
+        :param fuzzy: bool, optional (default: true), whether to perform fuzzy/approximate matching of
+            the query with the database entries' fields, using a custom implementation
+            of the edit distance algorithm.
+        :param includePoly: bool, optional (default: true), whether to search for matches in PolyModels
+            instead of just in anythings collection.
+        :param includeTags: bool, optional (default: true), whether to accept search results based on
+            query-tag matches.
+        :param includeTaxonomy: bool, optional (default: true), whether to accept search results based
+            on query-taxonomy matches.
+        :param includeCategories: bool, optional (default: true), whether to include a themeCategories
+            field in each returned entry. Those categories are calculated based on the tags, taxonomy
+            and entity field of entries. The possible categories are: “animal“, “architecture“,
+            “art”, “vehicle”, “character”, “culture”, “electronic”, “fashion”, “food”, “furniture”,
+            “music”, “nature”, “news”, “people”, “place”, “science”, “sports”, “weapon”,
+            plus a fall-back category “other“ used when none of the others is found suitable. 
+        :param vehicleOnly: bool, (default: false), whether to return only entries whose type is
+            vehicle-based. Only use it in search mode.
+        :param segmentedOnly: bool, (default: false), whether to return only entries that have segmented
+            parts (ignoring those that have a single part since they have no split). Only use it in search mode.
+        :param riggedOnly: bool, (default: false), whether to return only entries that have a valid rig.
+            Only use it in search mode.
+        :param animatedOnly: bool, (default: false), whether to return only entries that have a valid
+            rig with animations. Only use it in search mode.
+        :param sort: str in [“default“, “popularity“, “vote“, “lexicographical“], (default: “default“), how to
+            arrange search results. The default option applies query matching relevance, in the sense
+            that exact matches come first, then fuzzy matches with increasing edit distance. In the case of tie,
+            anythings results come before PolyModels results, and in case of tie there too, lexicographical
+            order is used. The popularity option gives results in order of popularity field, the vote one in
+            order of voteScore field, and the lexicographical option is self-explanatory.
+        :param descending: bool, (default: true), whether to provide a descending sort given the selected
+            sort criteria, ascending otherwise.
+        :param objWrap: bool, (default: false), wrap response in a top-level “models“ object instead of having
+            array as top-level element.
+        :param increasePopularity: bool, (default: false), whether to increase the popularity of the returned item,
+            only valid for the name mode.
+        :return: dict, the JSON response from the API decoded as a dict.
+        """
+        # Required params
+        data = {
+            "key": self.api_key,
+        }
+        # Optional params
+        if model_name:
+            data["name"] = model_name
+        if search_query:
+            data["search"] = search_query
+        if guid:
+            data["guid"] = guid
+        if fuzzy:
+            data["fuzzy"] = fuzzy
+        if include_poly:
+            data["includePoly"] = include_poly
+        if include_tags:
+            data["includeTags"] = include_tags
+        if include_taxonomy:
+            data["includeTaxonomy"] = include_taxonomy
+        if include_categories:
+            data["includeCategories"] = include_categories
+        if vehicle_only:
+            data["vehicleOnly"] = vehicle_only
+        if segmented_only:
+            data["segmentedOnly"] = segmented_only
+        if rigged_only:
+            data["riggedOnly"] = rigged_only
+        if animated_only:
+            data["animatedOnly"] = animated_only
+        if sort:
+            data["sort"] = sort
+        if descending:
+            data["descending"] = descending
+        if obj_wrap:
+            data["objWrap"] = obj_wrap
+        if increase_popularity:
+            data["increasePopularity"] = increase_popularity
+
+        return await send_request(
+            url=f"{self.api_url}/anything",
+            method="GET",
+            params=data
+        )
 
 
     async def animate(
