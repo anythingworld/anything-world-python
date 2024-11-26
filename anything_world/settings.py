00f55f469e805c0e04af3abab5ba95862d576c96
Vilson Vieira <vilson@void.cc>
2024-11-26 16:47:47 +0000

    Simplify env vars and settings

---
+++ anything_world/settings.py
@@ -0,0 +1,3 @@
+AW_API_URL = "https://api.anything.world"
+AW_POLLING_URL = "https://api.anything.world/user-processed-model"
+AW_GENERATED_POLLING_URL = "https://api.anything.world/user-generated-model"
